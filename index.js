c8fa9a130facd34880204bd725847253927150b8
Micheni58 <michenicaleb@gmail.com>
2025-06-11 20:02:50 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ");
+console.log(moo, neigh, baa, oink, cluck);
+
+const [bessie, ,dolly, babe,little] = farmAnimals.split(" ");
+
+const [blackAndWhite, ,black,pink] = farmAnimals.split(" ");
+
+
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+const [r, o, y, g, b, , v] = colors;
+console.log(r, o, y, g, b, v);
+
+const [,,,,,indg,] = colors;
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +25,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const {muppetName,color,song,job,partner} = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -25,9 +43,23 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
+const{
+  album:{
+    theMuppetMovie: {song2,song4}
+  },
+  nestedJob,
+  nestedPartner
+} = nestedMuppet;
+console.log(song2);
+console.log(song4);
+console.log(nestedJob);
+console.log(nestedPartner);
+// const{nestedJob, nestedPartner } = nestedMuppet.nestedJob&&nestedPartner;
+
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 
